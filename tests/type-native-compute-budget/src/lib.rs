c4180b3fe44a4c2ee0fdac68a7c23e65e9eb18f5
aklil <akliluyirgalem@gmail.com>
2026-02-08 23:16:54 +0300

    add test for compute budget

---
+++ tests/type-native-compute-budget/src/lib.rs
@@ -0,0 +1,23 @@
+use {
+    pincon::InstructionAccounts,
+    pinocchio::{entrypoint, error::ProgramError, AccountView, Address, ProgramResult},
+};
+
+#[derive(InstructionAccounts)]
+pub struct Noop<'view> {
+    #[pincon(signer)]
+    pub user: &'view AccountView,
+    #[pincon(type = native(compute_budget))]
+    pub vote_account: &'view AccountView,
+}
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Address,
+    accounts: &[AccountView],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let _ctx = Noop::try_from(accounts)?;
+    Ok(())
+}
